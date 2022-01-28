748181f3dce44c47342c5e7a6aebbdfc9952b2f1
EmmettRamm <ewramm@gmail.com>
2022-01-28 10:54:25 -0500

    Kind of working robot

--- generate.py
+++ generate.py
@@ -8,8 +8,8 @@ def Create_World():
     width = 1
     height = 1
 
-    x = 0
-    y = 0
+    x = 3
+    y = 3
     z = .5
 
     pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
@@ -19,9 +19,11 @@ def Create_World():
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
 
-    pyrosim.Send_Cube(name="Torso", pos=[1,1,.5] , size=[1,1,1])
-    pyrosim.Send_Joint( name = "Torso_Leg" , parent= "Torso" , child = "Leg" , type = "revolute", position = [.5,0,1])
-    pyrosim.Send_Cube(name="Leg", pos=[1.0,0,1.5], size=[1,1,1])
+    pyrosim.Send_Cube(name="Torso", pos=[0,1.5,1.5] , size=[1,1,1])
+    pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [0,2,1])
+    pyrosim.Send_Cube(name="FrontLeg", pos=[0,0.5,-0.5], size=[1,1,1])
+    pyrosim.Send_Joint( name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [0,-1,0])
+    pyrosim.Send_Cube(name="BackLeg", pos=[0,0,0], size=[1,1,1])
 
     pyrosim.End()
 
