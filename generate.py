3dc0ba5aca3b430a1580a8b89a0a16a7c1786fdc
EmmettRamm <ewramm@gmail.com>
2022-01-28 11:23:42 -0500

    Finished Assignment 2

--- generate.py
+++ generate.py
@@ -22,8 +22,8 @@ def Create_Robot():
     pyrosim.Send_Cube(name="Torso", pos=[0,1.5,1.5] , size=[1,1,1])
     pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [0,2,1])
     pyrosim.Send_Cube(name="FrontLeg", pos=[0,0.5,-0.5], size=[1,1,1])
-    pyrosim.Send_Joint( name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [0,-1,0])
-    pyrosim.Send_Cube(name="BackLeg", pos=[0,0,0], size=[1,1,1])
+    pyrosim.Send_Joint( name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [0,1,1])
+    pyrosim.Send_Cube(name="BackLeg", pos=[0,-0.5,-0.5], size=[1,1,1])
 
     pyrosim.End()
 
