3d4eea224d6c1e1cca9568394f0add2d64dff306
EmmettRamm <ewramm@gmail.com>
2022-02-13 11:18:58 -0500

    output sin values from -1 to 1. trying to figure out how to get it between -pi/4 , +pi/4

--- analyze.py
+++ analyze.py
@@ -1,12 +1,14 @@
 import numpy
-import matplotlib.pyplot
+import matplotlib.pyplot as pPlot
 
 backLegSensorValues = numpy.load("data/backLegSensorData.npy")
 frontLegSensorValues = numpy.load("data/frontLegSensorData.npy")
+sinVectorValues = numpy.load("data/sinvectordata.npy")
 print(backLegSensorValues)
 print(frontLegSensorValues)
 
-matplotlib.pyplot.plot(backLegSensorValues, label="backLeg", linewidth=4)
-matplotlib.pyplot.plot(frontLegSensorValues, label="frontLeg")
-matplotlib.pyplot.legend()
-matplotlib.pyplot.show()
+#matplotlib.pyplot.plot(backLegSensorValues, label="backLeg", linewidth=4)
+#matplotlib.pyplot.plot(frontLegSensorValues, label="frontLeg")
+pPlot.plot(sinVectorValues, label="Sin Vector")
+pPlot.legend()
+pPlot.show()
--- simulation.py
+++ simulation.py
@@ -3,21 +3,39 @@ import pybullet_data
 import time
 import pyrosim.pyrosim as pyrosim
 import numpy
+import random
 
+#setting gravity
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
+
+#loading in the floor plane, the robot, and the world
 planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 
+#simplifying pi
+pi = numpy.pi
+
+#initializing the loop length
 loop = 1000
 
+targetAngles = numpy.sin(numpy.array(numpy.linspace(0, 2*pi, loop)))
+
+numpy.save("data/sinvectordata.npy", targetAngles)
+exit()
+
+#setting up sensors
 backLegSensorValues = numpy.zeros(loop)
 frontLegSensorValues = numpy.zeros(loop)
 print(backLegSensorValues)
 print(frontLegSensorValues)
+
+#preping to simulate the robot
 pyrosim.Prepare_To_Simulate(robotId)
+
+#loop for simulation
 for i in range(loop):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
@@ -27,8 +45,15 @@ for i in range(loop):
     bodyIndex = robotId,
     jointName = "Torso_BackLeg",
     controlMode = p.POSITION_CONTROL,
-    targetPosition = 0.0,
-    maxForce = 500)
+    targetPosition = random.uniform(-pi/2.0, pi/2.0),
+    maxForce = 50)
+
+    pyrosim.Set_Motor_For_Joint(
+    bodyIndex = robotId,
+    jointName = "Torso_FrontLeg",
+    controlMode = p.POSITION_CONTROL,
+    targetPosition = random.uniform(-pi/2.0, pi/2.0),
+    maxForce = 50)
 
     time.sleep(1/30)
     
